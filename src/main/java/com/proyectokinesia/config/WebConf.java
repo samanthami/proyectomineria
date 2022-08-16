c09eb141baaa3e54081800252015a1a8b19e3254
Samantha Jara <milensamy11@gmail.com>
2022-08-16 16:39:36 -0500

    prueba deploy

---
+++ src/main/java/com/proyectokinesia/config/WebConf.java
@@ -0,0 +1,17 @@
+package com.proyectokinesia.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConf implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*").allowCredentials(true)
+                .allowedMethods("*")
+                .maxAge(3600);
+    }
+}
