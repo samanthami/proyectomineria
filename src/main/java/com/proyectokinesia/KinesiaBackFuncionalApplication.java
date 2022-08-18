b6a2f16b73a621d66fe67ef6699cd793d4eb445e
Samantha Jara <milensamy11@gmail.com>
2022-08-18 12:49:36 -0500

    "sirve no tocar x4"

---
+++ src/main/java/com/proyectokinesia/KinesiaBackFuncionalApplication.java
@@ -0,0 +1,13 @@
+package com.proyectokinesia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinesiaBackFuncionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinesiaBackFuncionalApplication.class, args);
+    }
+
+}
